9b12e4daf6199526a880e4c72755fd36ac3efbb4
Jan Jakubik <janekjak@gmail.com>
2021-07-06 22:21:54 +0200

    Fix missing files

---
+++ src/web/app/domain/user/registration/registration-view/registration-view.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-registration-view',
+  templateUrl: './registration-view.component.html',
+  styleUrls: ['./registration-view.component.css']
+})
+export class RegistrationViewComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
